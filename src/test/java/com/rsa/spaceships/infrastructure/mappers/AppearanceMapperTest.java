8a29d1f4dfc7c6def47ee0fb1457f9867c4be280
Roberto Sánchez <rsa568422@gmail.com>
2024-06-15 23:38:35 +0200

    Se agregan tests unitarios para los mappers del paquete application

---
+++ src/test/java/com/rsa/spaceships/infrastructure/mappers/AppearanceMapperTest.java
@@ -0,0 +1,73 @@
+package com.rsa.spaceships.infrastructure.mappers;
+
+import com.rsa.spaceships.Data;
+import com.rsa.spaceships.domain.models.Appearance;
+import com.rsa.spaceships.infrastructure.entities.AppearanceEntity;
+import com.rsa.spaceships.infrastructure.entities.SpaceshipEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class AppearanceMapperTest {
+
+    @Autowired
+    private AppearanceMapper mapper;
+
+    @Test
+    void toAppearance() {
+        var entity = toAppearanceEntity(1, Data.getExpectedSpaceshipEntity(1));
+        var expected = new Appearance(1, Data.getExpectedSpaceship(1), Data.getExpectedRecording(1));
+
+        var actual = mapper.toAppearance(entity);
+
+        assertAll(
+                () -> assertNotNull(actual),
+                () -> assertEquals(expected, actual)
+        );
+    }
+
+    @Test
+    void toAppearances() {
+        var entities = Data.EXPECTED_RECORDINGS.keySet()
+                .stream()
+                .map(id ->  toAppearanceEntity(id, Data.getExpectedSpaceshipEntity(1)))
+                .toList();
+        var expected = Data.EXPECTED_RECORDINGS.keySet()
+                .stream()
+                .map(id ->  new Appearance(id, Data.getExpectedSpaceship(1), Data.getExpectedRecording(id)))
+                .toList();
+
+        var actual = mapper.toAppearances(entities);
+
+        assertAll(
+                () -> assertNotNull(actual),
+                () -> assertEquals(expected, actual)
+        );
+    }
+
+    @Test
+    void toAppearanceEntity() {
+        var appearance = new Appearance(1, Data.getExpectedSpaceship(1), Data.getExpectedRecording(1));
+        var expected = toAppearanceEntity(1, Data.getExpectedSpaceshipEntity(1));
+
+        var actual = mapper.toAppearanceEntity(appearance);
+
+        assertAll(
+                () -> assertNotNull(actual),
+                () -> assertEquals(expected, actual)
+        );
+    }
+
+    private static AppearanceEntity toAppearanceEntity(Integer id, SpaceshipEntity spaceshipEntity) {
+        var appearanceEntity = new AppearanceEntity();
+        appearanceEntity.setId(id);
+        appearanceEntity.setSpaceship(spaceshipEntity);
+        appearanceEntity.setRecording(Data.getExpectedRecordingEntity(id));
+        return appearanceEntity;
+    }
+}
---
+++ src/test/java/com/rsa/spaceships/infrastructure/mappers/SpaceshipMapperTest.java
@@ -0,0 +1,72 @@
+package com.rsa.spaceships.infrastructure.mappers;
+
+import com.rsa.spaceships.Data;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class SpaceshipMapperTest {
+
+    @Autowired
+    private SpaceshipMapper mapper;
+
+    @Test
+    void toSpaceship() {
+        var entity = Data.getExpectedSpaceshipEntity(1);
+        var expected = Data.getExpectedSpaceship(1);
+
+        var actual = mapper.toSpaceship(entity);
+
+        assertAll(
+                () -> assertNotNull(actual),
+                () -> assertEquals(expected, actual)
+        );
+    }
+
+    @Test
+    void toSpaceshipsIterable() {
+        var entities = Data.EXPECTED_SPACESHIPS.keySet().stream().map(Data::getExpectedSpaceshipEntity).toList();
+        var expected = Data.EXPECTED_SPACESHIPS.keySet().stream().map(Data::getExpectedSpaceship).toList();
+
+        var actual = mapper.toSpaceships(entities);
+
+        assertAll(
+                () -> assertNotNull(actual),
+                () -> assertIterableEquals(expected, actual)
+        );
+    }
+
+    @Test
+    void toSpaceshipsPage() {
+        var entities = Data.EXPECTED_SPACESHIPS.keySet().stream().map(Data::getExpectedSpaceshipEntity).toList();
+        var expected = Data.EXPECTED_SPACESHIPS.keySet().stream().map(Data::getExpectedSpaceship).toList();
+
+        var actual = mapper.toSpaceships(new PageImpl<>(entities, PageRequest.of(0, 10), 5));
+
+        assertAll(
+                () -> assertNotNull(actual),
+                () -> assertIterableEquals(expected, actual)
+        );
+    }
+
+    @Test
+    void toSpaceshipEntity() {
+        var spaceship = Data.getExpectedSpaceship(1);
+        var expected = Data.getExpectedSpaceshipEntity(1);
+
+        var actual = mapper.toSpaceshipEntity(spaceship);
+
+        assertAll(
+                () -> assertNotNull(actual),
+                () -> assertEquals(expected, actual)
+        );
+    }
+}
