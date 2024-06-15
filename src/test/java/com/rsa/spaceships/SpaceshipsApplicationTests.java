5c833686279793a88f39cb5f3f73f36673cecf1a
Roberto Sánchez <rsa568422@gmail.com>
2024-06-15 13:03:20 +0200

    Initial commit

---
+++ src/test/java/com/rsa/spaceships/SpaceshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsa.spaceships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
