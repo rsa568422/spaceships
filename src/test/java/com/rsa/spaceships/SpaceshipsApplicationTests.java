88451c4ae9bcd4aef4066208c783e8089fbb50a3
Roberto Sánchez <rsa568422@gmail.com>
2024-06-16 03:10:31 +0200

    Test para SpaceshipsApplication

--- src/test/java/com/rsa/spaceships/SpaceshipsApplicationTests.java
+++ src/test/java/com/rsa/spaceships/SpaceshipsApplicationTests.java
@@ -1,13 +1,17 @@
 package com.rsa.spaceships;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 class SpaceshipsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertDoesNotThrow(() -> SpringApplication.run(SpaceshipsApplication.class, ""));
 	}
 
 }
